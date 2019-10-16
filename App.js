51c64298599997dce16242bd0a18133c9098e6ae
rinkurock <rinku.arnob@gmail.com>
2019-10-17 02:32:48 +0600

    woking on event and state

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.layout}>
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="New task"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <View>
+        {courseGoals.map(goal => (
+          <Text>{goal}</Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  layout: {
+    //flex: 1,
+    backgroundColor: "#fff",
+    padding: 100
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%"
+  },
+  inputBox: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
